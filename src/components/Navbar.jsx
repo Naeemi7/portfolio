3bb85a830a2276cbe232ac2650b04091e3fd6d22
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-29 01:32:04 +0200

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,10 @@ function Navbar() {
       <nav className="bg-primary border-gray-200 dark:bg-primary">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="#" className="flex items-center">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span
+              className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+              style={{ fontFamily: "Pacifico, cursive" }}
+            >
               Abdulwase Naeemi
             </span>
           </a>
