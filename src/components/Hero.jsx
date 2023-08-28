92fe995aea345a8c5f58bcda33efcfaf384319d6
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-28 23:48:40 +0200

    new changes has been made

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,13 @@ function Hero() {
       <section className="bg-white dark:bg-primary h-sreen">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Abdulwase Naeemi Full Stack Web Developer
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-h1Color">
+              Abdulwase Naeemi
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            <h2 className="text-4xl font-extrabold dark:text-h2Color mb-4">
+              Payments tool for companies
+            </h2>
+            <p className="max-w-2xl mb-6 font-light text- lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               From checkout to global sales tax compliance, companies around the
               world use Flowbite to simplify their payment stack.
             </p>
