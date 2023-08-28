0aa66ad48846e2752138a9bcdd440fead389c233
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-29 00:34:25 +0200

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./styles/App.scss";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Project from "./components/Project";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Project />
     </>
   );
 }
---
+++ src/components/Project.jsx
@@ -0,0 +1,9 @@
+function Project() {
+  return (
+    <div>
+      <h1>I am Project</h1>
+    </div>
+  );
+}
+
+export default Project;
