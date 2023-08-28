faf7e71ccf9015e705efdbb7a0afe987d3468450
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-28 23:12:36 +0200

    bug fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 function Hero() {
   return (
     <>
-      <section className="bg-white dark:bg-primary">
+      <section className="bg-white dark:bg-primary h-sreen">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Payments tool for software companies
+              Abdulwase Naeemi Full Stack Web Developer
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               From checkout to global sales tax compliance, companies around the
