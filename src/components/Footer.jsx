82aa9d662a2bb9eb09541499b748cb2aa186f14a
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-29 14:52:59 +0200

    bug fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,65 +1,37 @@
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
-import logo from "../assets/images/footer/logo.png";
 
 function Footer() {
-  const SocialMediaLink = ({ href, icon, srText }) => (
-    <li>
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-h2Color hover:text-gray-900 dark:hover:text-h1Color flex items-center space-x-2"
-      >
-        {icon}
-        <span className="sr-only">{srText}</span>
-      </a>
-    </li>
-  );
-
   return (
-    <footer className="bg-white dark:bg-primary">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 flex flex-col md:flex-row md:justify-between">
-        <div className="mb-6 md:mb-0 flex items-center md:order-1 order-2">
-          <a href="https://flowbite.com/" className="flex items-center">
-            <img
-              src={logo}
-              className="h-14 w-auto max-w-xs"
-              alt="FlowBite Logo"
-            />
-          </a>
-        </div>
-        <div className="md:order-2 order-1">
-          <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-            Follow Me
-          </h2>
-          <ul className="text-gray-500 dark:text-gray-400 font-medium flex space-x-5 sm:justify-center sm:mt-0">
-            <SocialMediaLink
-              href="#"
-              icon={<FaFacebook className="w-9 h-9 shadow-md" />}
-              srText="Facebook page"
-            />
-            <SocialMediaLink
-              href="https://github.com/Naeemi7"
-              icon={<FaGithub className="w-9 h-9 shadow-md" />}
-              srText="GitHub account"
-            />
-            <SocialMediaLink
-              href="https://www.linkedin.com/in/abdulwase-naeemi/"
-              icon={<FaLinkedin className="w-9 h-9 shadow-md" />}
-              srText="LinkedIn account"
-            />
-          </ul>
-        </div>
-      </div>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div className="flex items-center justify-center">
-        <span className="text-sm text-gray-500 dark:text-gray-400 mb-4">
+    <footer className="bg-primary border-gray-200 dark:bg-primary">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="#" className="hover:underline">
-            Abdulwase Naeemi™
+          <a href="https://flowbite.com/" className="hover:underline">
+            Flowbite™
           </a>
           . All Rights Reserved.
         </span>
+
+        <div className="flex items-center mt-3 md:mt-0">
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-700 mr-4 text-xl md:text-2xl"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-700 mr-4 text-xl md:text-2xl"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-700 text-xl md:text-2xl"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
       </div>
     </footer>
   );
