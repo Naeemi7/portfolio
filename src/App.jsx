dc85b8f3bb6e226240101a67b4fbed4e134b71bd
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-29 12:39:30 +0200

    new changes has been made

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./styles/App.scss";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Project from "./components/Project";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Project />
+      <Contact />
+      {/*   <Project /> */}
     </>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,71 @@
+function Contact() {
+  return (
+    <>
+      <section className="bg-white dark:bg-white">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-10 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-primary">
+            Contact Me
+          </h2>
+          <p className="mb-10 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-600 sm:text-xl">
+            Got a technical issue? Want to send feedback about a beta feature?
+            Need details about our Business plan? Let us know.
+          </p>
+          <form action="#" className="space-y-8">
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="name@flowbite.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Let us know how we can help you"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600"
+              >
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows="6"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a comment..."
+              />
+            </div>
+            <button
+              type="submit"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-buttonColor hover:bg-h1Color hover:text-gray-600 focus:bg-h2Color focus:text-white"
+            >
+              Send message
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Contact;
