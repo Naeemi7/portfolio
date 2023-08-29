360aed14e655509eac559cb9fbabc9c944f862aa
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-29 14:20:46 +0200

    footer has been finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,31 @@
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
+import logo from "../assets/images/footer/logo.png";
 
 function Footer() {
+  const SocialMediaLink = ({ href, icon, srText }) => (
+    <li>
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-h2Color hover:text-gray-900 dark:hover:text-h1Color flex items-center space-x-2"
+      >
+        {icon}
+        <span className="sr-only">{srText}</span>
+      </a>
+    </li>
+  );
+
   return (
     <footer className="bg-white dark:bg-primary">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 flex flex-col md:flex-row md:justify-between">
         <div className="mb-6 md:mb-0 flex items-center md:order-1 order-2">
           <a href="https://flowbite.com/" className="flex items-center">
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8 mr-3"
+              src={logo}
+              className="h-14 w-auto max-w-xs"
               alt="FlowBite Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
           </a>
         </div>
         <div className="md:order-2 order-1">
@@ -22,50 +33,21 @@ function Footer() {
             Follow Me
           </h2>
           <ul className="text-gray-500 dark:text-gray-400 font-medium flex space-x-5 sm:justify-center sm:mt-0">
-            <li>
-              <a
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-h2Color hover:text-gray-900 dark:hover:text-h1Color flex items-center space-x-2"
-              >
-                <FaFacebook className="w-9 h-9 shadow-md" />
-                <span className="sr-only">Facebook page</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-h2Color hover:text-gray-900 dark:hover:text-h1Color flex items-center space-x-2"
-              >
-                <FaGithub className="w-9 h-9 shadow-md" />
-                <span className="sr-only">GitHub account</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/abdulwase-naeemi/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-h2Color hover:text-gray-900 dark:hover:text-h1Color flex items-center space-x-2"
-              >
-                <FaLinkedin className="w-9 h-9 shadow-md" />
-                <span className="sr-only">LinkedIn account</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="mailto:abdulwasenaeemi7@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-h2Color hover:text-gray-900 dark:hover:text-h1Color flex items-center space-x-2"
-              >
-                <SiGmail className="w-9 h-9 shadow-md" />
-                <span className="sr-only">Email</span>
-              </a>
-            </li>
+            <SocialMediaLink
+              href="#"
+              icon={<FaFacebook className="w-9 h-9 shadow-md" />}
+              srText="Facebook page"
+            />
+            <SocialMediaLink
+              href="#"
+              icon={<FaGithub className="w-9 h-9 shadow-md" />}
+              srText="GitHub account"
+            />
+            <SocialMediaLink
+              href="https://www.linkedin.com/in/abdulwase-naeemi/"
+              icon={<FaLinkedin className="w-9 h-9 shadow-md" />}
+              srText="LinkedIn account"
+            />
           </ul>
         </div>
       </div>
