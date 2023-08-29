6d7bb3a9878eb0f90e141057c3fedee41d1eded3
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-29 14:26:02 +0200

    bug fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
           <h2 className="mb-10 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-primary">
             Contact Me
           </h2>
-          <p className="mb-10 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-600 sm:text-xl">
+          <p className="mb-10 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-600 sm:text-xl">
             If you are interested in learning more about me or my work, or if
             you just want to reach out and say hello, feel free to send me a
             message. Im excited to connect with you and hear your thoughts!
@@ -22,7 +22,7 @@ function Contact() {
               <input
                 type="text"
                 id="name"
-                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-primary dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Enter your name"
                 required
               />
@@ -37,7 +37,7 @@ function Contact() {
               <input
                 type="email"
                 id="email"
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-primary dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Enter your email"
                 required
               />
@@ -53,7 +53,7 @@ function Contact() {
               <textarea
                 id="message"
                 rows="6"
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-primary dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
               />
             </div>
