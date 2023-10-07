d3054e31fc6e763ba47ce71cdebe42048a46d72e
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-07 15:58:41 +0200

    bug fixed

--- src/components/Main/Skills.jsx
+++ src/components/Main/Skills.jsx
@@ -3,22 +3,13 @@ import skillAImages from "../../data/skill-images";
 
 function Skills() {
   return (
-    <div className="about-container">
-      <div className="about-wrapper">
+    <div className="skills-main-container">
+      <div className="skills-sub-container">
         <section className="content-container">
           <h2>My Skills</h2>
-          {/*    <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-            tempora iste est cum, error reprehenderit dolore rem, repudiandae
-            fuga aut nulla ratione. Minima a sed accusantium, quod incidunt
-            corrupti commodi error, praesentium, velit libero laboriosam? Sequi
-            ipsa consectetur repellat, accusamus aliquid laborum totam ea facere
-            officiis beatae alias harum non, corporis quam similique aliquam?
-            Minima ullam quam fuga consectetur nobis illo voluptate obcaecati,
-          </p> */}
         </section>
-
-        <section className="skill-container">
+        <h3>Technical Skills</h3>
+        <section className="skill-wrapper">
           {skillAImages.map((item, index) => (
             <div className="skill" key={index}>
               <img
