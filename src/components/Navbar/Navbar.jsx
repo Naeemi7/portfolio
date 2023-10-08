ef6c3a11ca41057bb6d4dfcc6f015ea7c04be085
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-08 22:17:15 +0200

    the navbar has been modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -80,14 +80,6 @@ function Navbar() {
                   href="#"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-h1Color md:p-0 dark:text-white md:dark:hover:text-h1Color dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Resume
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-h1Color md:p-0 dark:text-white md:dark:hover:text-h1Color dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
                   Contact
                 </a>
               </li>
