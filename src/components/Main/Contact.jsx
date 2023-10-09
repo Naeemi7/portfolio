2e2bac5576770c7a0a68f1977b86a2bc72b7b814
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-09 07:33:33 +0200

    the background of contact page has been changed

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -6,49 +6,49 @@ function Contact() {
   return (
     <>
       <SectionSeperator />
-      <section className="min-h-screen bg-white dark:bg-primary">
+      <section className="min-h-screen bg-white dark:bg-secondary">
         <div className="container px-6 py-10 mx-auto">
           <div className="lg:flex lg:items-center lg:-mx-10">
             <div className="lg:w-1/2 lg:mx-10">
-              <h1 className="text-2xl font-semibold text-gray-800 capitalize dark:text-white lg:text-3xl">
+              <h1 className="text-2xl font-semibold text-gray-800 capitalize dark:text-h2Color lg:text-3xl">
                 Let’s talk
               </h1>
 
-              <p className="mt-4 text-gray-500 dark:text-gray-400">
+              <p className="mt-4 text-gray-500 dark:text-gray-900">
                 Ask me everything, and I would love to hear from you
               </p>
 
               <form className="mt-12">
                 <div className="-mx-2 md:items-center md:flex">
                   <div className="flex-1 px-2">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-700">
                       Full Name
                     </label>
                     <input
                       type="text"
                       placeholder="John Doe"
-                      className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-primary dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                      className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-00 bg-white border border-gray-600 rounded-md dark:placeholder-gray-600 dark:bg-secondary dark:text-gray-6000 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                   </div>
 
                   <div className="flex-1 px-2 mt-4 md:mt-0">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-700">
                       Email address
                     </label>
                     <input
                       type="email"
                       placeholder="johndoe@example.com"
-                      className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-primary dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                      className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-secondary dark:text-gray-600 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                   </div>
                 </div>
 
                 <div className="w-full mt-4">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-700">
                     Message
                   </label>
                   <textarea
-                    className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-56 dark:placeholder-gray-600 dark:bg-primary dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-56 dark:placeholder-gray-600 dark:bg-secondary dark:text-gray-600 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                     placeholder="Message"
                   ></textarea>
                 </div>
@@ -70,7 +70,7 @@ function Contact() {
                 <p className="flex items-start -mx-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400"
+                    className="w-6 h-6 mx-2 text-blue-500 dark:text-buttonColor"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -88,7 +88,7 @@ function Contact() {
                     />
                   </svg>
 
-                  <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">
+                  <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-700">
                     49808, Lingen (EMS), Germany
                   </span>
                 </p>
@@ -96,7 +96,7 @@ function Contact() {
                 <p className="flex items-start -mx-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400"
+                    className="w-6 h-6 mx-2 text-blue-500 dark:text-buttonColor"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -109,7 +109,7 @@ function Contact() {
                     />
                   </svg>
 
-                  <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">
+                  <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-700">
                     (+49) 176 7196 1939
                   </span>
                 </p>
@@ -117,7 +117,7 @@ function Contact() {
                 <p className="flex items-start -mx-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400"
+                    className="w-6 h-6 mx-2 text-blue-500 dark:text-buttonColor"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -130,14 +130,14 @@ function Contact() {
                     />
                   </svg>
 
-                  <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">
+                  <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-700">
                     abdulwasenaeemi7@gmail.com
                   </span>
                 </p>
               </div>
 
               <div className="mt-6 w-80 md:mt-8">
-                <h3 className="text-gray-600 dark:text-gray-300 mb-3">
+                <h3 className="text-gray-600 dark:text-gray-900 mb-3">
                   Follow me
                 </h3>
 
