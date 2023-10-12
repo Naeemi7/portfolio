a07c960ed5f7f3c120fada0c1d20163226d19dda
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-12 19:04:09 +0200

    bug fixeD

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import SocialMediaIcons from "../Helper/SocialMediaIcons";
 
 function Footer() {
   return (
-    <footer className="bg-primary border-gray-200 dark:bg-primary">
+    <footer className="bg-primary border-gray-200 dark:bg-primary fixed bottom-0 w-full">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
